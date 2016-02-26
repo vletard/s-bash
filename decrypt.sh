8ff3d918ebf22d460c25ba83d4fb1444c81e7027
vincent letard <vincent.letard@limsi.fr>
2016-02-26 15:24:04 +0100

    Gestion de stdin pour decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -3,7 +3,12 @@
 # Déchiffre chaque fichier fourni en argument
 # Le déchiffrage est asymétrique et cherche parmi les clés privées gpg disponibles
 
-for f in $*
-do
-  gpg --decrypt $f | tar xvz
-done
+if (( $# == 0 ))
+then
+    gpg --decrypt | tar xvz
+else
+  for f in $*
+  do
+    gpg --decrypt $f | tar xvz
+  done
+fi
