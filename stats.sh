53cf300e21ea34fcc0fd743a64c094ed665d5346
vincent <vince.letard@laposte.net>
2017-12-13 17:06:39 +0100

    stats.sh check for presence of required R executable before launch

--- stats.sh
+++ stats.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! which R > /dev/null
+then
+  printf "%s requires R to be installed.\naborting.\n" "$0" >&2
+  exit 1
+fi
+
 if (( $# == 0 ))
 then
   R -q -e "x <- read.csv('stdin', header = F); summary(x); cat(sprintf(' StDev  :%f\n Length :%d\n   Sum  :%f\n', sd(x[ , 1]), length(x[, 1]), sum(x[,1])))"
